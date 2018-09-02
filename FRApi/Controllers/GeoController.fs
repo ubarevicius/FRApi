0569d09abc4a8c597e56a751da8b31fe4d485cf1
Matas Ubarevicius <m.ubarevicius@gmail.com>
2018-09-02 19:46:15 +0200

    Added api services and C# RhinoCompute project for now

---
+++ FRApi/Controllers/GeoController.fs
@@ -0,0 +1,40 @@
+﻿namespace RApi.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Rhino.Compute
+
+[<Route("api/[controller]")>]
+[<ApiController>]
+type GeoController () =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member this.Get() =
+        let apiToken = "matas@ubarevicius.lt"
+        ComputeServer.ApiToken <- apiToken
+        let sphere = new Rhino.Geometry.Sphere(Rhino.Geometry.Point3d.Origin, 12.0);
+        let sphereAsBrep = sphere.ToBrep();
+        let meshes = MeshCompute.CreateFromBrep(sphereAsBrep);
+        let values ="The mesh has " + meshes.First().Vertices.Count.ToString() + " vertices"
+        ActionResult<string>(values)
+
+    [<HttpGet("{id}")>]
+    member this.Get(id:int) =
+        let value = "value"
+        ActionResult<string>(value)
+
+    [<HttpPost>]
+    member this.Post([<FromBody>] value:string) =
+        ()
+
+    [<HttpPut("{id}")>]
+    member this.Put(id:int, [<FromBody>] value:string ) =
+        ()
+
+    [<HttpDelete("{id}")>]
+    member this.Delete(id:int) =
+        ()
